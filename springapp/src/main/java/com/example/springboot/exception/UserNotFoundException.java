f2ac918b3246a8bc07c363c9e96c16686d0c169d
vamsimopuri <111500638+vamsimopuri@users.noreply.github.com>
2022-10-12 16:43:11 +0530

    Delete springapp directory

--- springapp/src/main/java/com/example/springboot/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.exception;
-
-public class UserNotFoundException extends RuntimeException{
-	public UserNotFoundException(Long id) {
-		super("Could not found the user with id"+id);
-	}
-
-}
--- springapp/src/main/java/com/example/springboot/services/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminService {
-
-}
